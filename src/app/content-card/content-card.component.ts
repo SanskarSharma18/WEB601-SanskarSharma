7e8a77d7eb814dac62147155968be22923624509
SanskarSharma18 <136133385+SanskarSharma18@users.noreply.github.com>
2024-02-24 19:19:15 -0500

    Add files via upload

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,48 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.css'
 })
 
 export class ContentCardComponent {
-
-  contentList: ContentList = new ContentList();
-
-  constructor() { 
-
-    this.contentList.add({
-      id: 1,
-      title: 'Mars',
-      description: 'The fourth planet from the Sun in our solar system.',
-      creator: 'NASA',
-      imgURL: 'mars.jpg',
-      type: 'Planet',
-      tags: ['Red Planet', 'Exploration']
-    });
-
-    this.contentList.add({
-      id: 2,
-      title: 'Jupiter',
-      description: 'The largest planet in our solar system, known for its giant storms.',
-      creator: 'European Space Agency (ESA)',
-      imgURL: 'jupiter.jpg',
-      type: 'Planet',
-      tags: ['Gas Giant', 'Storms']
-    });
-
-    this.contentList.add({
-      id: 3,
-      title: 'Voyager 1',
-      description: 'A space probe launched by NASA in 1977. It has reached interstellar space.',
-      creator: 'NASA',
-      imgURL: 'voyager.jpg',
-      type: 'Space Probe',
-      tags: ['Interstellar', 'NASA Mission']
-    });
+  @Input() content: any;
+  @Input() borderStyle: string = '1px solid #000';  
+  displayInfo() {
+    console.log(`ID: ${this.content.id}, Title: ${this.content.title}`);
+  }
+  handleClick() {
+    console.log('Image clicked:', this.content.id, this.content.title);
+    // Add any other logic you want to execute on click
   }
 }
